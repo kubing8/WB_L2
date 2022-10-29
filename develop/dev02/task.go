ec5110e9e7ff6ccf7275761d7a221d721342c7b2
Skripalschikov Ivan <kubing8@gmail.com>
2022-10-29 14:46:36 +0300

    task 2 complete

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -19,5 +28,71 @@ package main
 */
 
 func main() {
+	instr := "eh2k2l\\4n\\\\2dg2"
+	outstr, err := decompos(instr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Входная строка: %q\nВыхоная строка: %q\n", instr, outstr)
+}
 
+func decompos(str string) (string, error) {
+	answer := strings.Builder{}
+	for ind, sym := range str {
+		//fmt.Println("LOG: ", string(sym))
+		if unicode.IsDigit(sym) {
+			if ind == 0 {
+				return "", errors.New("Incorrect string")
+			}
+
+			ch := str[ind-1]
+			if unicode.IsDigit(rune(ch)) {
+				continue
+			}
+			if ch == '\\' {
+				if str[ind-2] != '\\' {
+					num := searchNum(str, ind+1)
+					if num == 0 {
+						answer.WriteString(string(str[ind]))
+					} else {
+						for i := 0; i < num; i++ {
+							answer.WriteString(string(str[ind]))
+						}
+					}
+					continue
+				}
+			}
+
+			num := searchNum(str, ind)
+			for i := 0; i < num; i++ {
+				answer.WriteString(string(ch))
+			}
+		} else if str[ind] == '\\' {
+			continue
+		} else if ind == len(str)-1 || !unicode.IsDigit(rune(str[ind+1])) {
+			answer.WriteString(string(str[ind]))
+		}
+	}
+
+	return answer.String(), nil
+}
+
+func searchNum(str string, startInd int) int {
+	// Узнаем какое это число
+	count := 0
+	//fmt.Print("LOG: ", str[ind:])
+	for _, n := range str[startInd:] {
+		//fmt.Println(" num: ", string(n))
+		if unicode.IsDigit(n) {
+			count++
+		} else {
+			break
+		}
+	}
+
+	//fmt.Println(" count: ", count)
+	num, _ := strconv.Atoi(str[startInd : startInd+count])
+	//fmt.Println(" num: ", num)
+	return num
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestDecopositionString(t *testing.T) {
+	m := make(map[string]string)
+	m["a4bc2d5e"] = "aaaabccddddde"
+	m["abcd"] = "abcd"
+	m["45"] = ""
+	m[""] = ""
+
+	// На вход нужно указывать двойные облатные слэши -> на выходе тоже получаем двойные
+	m["ed\\\\2se"] = "ed\\\\se" // "ed\\2se" => "ed\\se"
+	m["ed\\4se"] = "ed4se"
+	m["ed\\\\4se"] = "ed\\\\\\\\se"
+
+	m["qwe\\4\\5"] = "qwe45"
+	m["qwe\\45"] = "qwe44444"
+	m["qwe\\\\2"] = "qwe\\\\"
+
+	m["eh2k2l\\4n\\\\2dg2"] = "ehhkkl4n\\\\dgg"
+
+	for k, v := range m {
+		if s, _ := decompos(k); s != v {
+			t.Error("Error in string: ", k)
+		}
+	}
+}
