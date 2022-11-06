f93d6b9bc8a3987bf92c9d97738bebd64c0761d2
Skripalschikov Ivan <kubing8@gmail.com>
2022-11-06 13:40:01 +0300

    task 6 complete

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +22,97 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Flags - флаги, поддерживаемые уилитой cut
+type Flags struct {
+	Fields    string
+	Delimiter string
+	Separated bool
+}
 
+func main() {
+	var (
+		fFlag = flag.String("f", "", "MUST BE! Choose column -- print only selected column")
+		dFlag = flag.String("d", "\t", "Used to select the DELIM")
+		sFlag = flag.Bool("s", false, "Doesn't print lines without DELIM")
+	)
+	flag.Parse()
+	fl := Flags{Fields: *fFlag, Delimiter: *dFlag, Separated: *sFlag}
+
+	var fileName string
+	if flag.Arg(0) != "." {
+		fileName = flag.Arg(0)
+	} else {
+		fileName = "C:\\GoCode\\src\\WB_L2\\develop\\dev06\\startText.txt"
+	}
+
+	data := readFile(fileName)
+
+	outData := cutFile(data, fl)
+	for _, str := range outData {
+		fmt.Println(str)
+	}
+}
+
+func readFile(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error open file!\n", err)
+	}
+	defer file.Close()
+
+	var text []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+	return text
+}
+
+func cutFile(data []string, flags Flags) []string {
+	selectedFields, err := parseFlagField(flags)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	d := "\t"
+	if flags.Delimiter != d {
+		d = flags.Delimiter
+	}
+
+	resStr := []string{}
+	for _, line := range data {
+		if d != "" && strings.Contains(line, d) {
+			words := strings.Split(line, d)
+
+			cutLine := strings.Builder{}
+
+			for _, val := range selectedFields {
+				if len(words) >= val {
+					cutLine.WriteString(words[val-1])
+					cutLine.WriteString(d)
+				}
+			}
+
+			// trim extra delimiter
+			resStr = append(resStr, strings.TrimSuffix(cutLine.String(), d))
+
+		} else if !flags.Separated {
+			resStr = append(resStr, line)
+		}
+	}
+	return resStr
+}
+
+func parseFlagField(flags Flags) ([]int, error) {
+	tmp := strings.Split(flags.Fields, ",")
+	fields := make([]int, len(tmp))
+
+	for i := range tmp {
+		num, err := strconv.Atoi(tmp[i])
+		if err != nil || num == 0 {
+			return nil, fmt.Errorf("can't convert string to int: %v", err)
+		}
+		fields[i] = num
+	}
+	return fields, nil
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,46 @@
 package main
+
+import "testing"
+
+type testStruct struct {
+	flags   Flags
+	outData []string
+}
+
+func TestCut(t *testing.T) {
+	inputText := readFile("C:\\GoCode\\src\\WB_L2\\develop\\dev06\\startText.txt")
+	tests := []testStruct{}
+
+	tempFl := Flags{"1,2", "", false}
+	tempOD := []string{
+		"drwxr-xr-x      6",
+		"drwx------      3",
+		"drwxr-xr-x      12",
+		"drwx------      5",
+		"drwxr-xr-x      7",
+		"drwx------      5",
+		"drwxr-xr-x      6",
+		"drwxr-xr-x      7",
+		"drwxr-xr-x      7",
+		"drwxr-xr-x      7",
+		"drwxr-xr-x      8",
+		"drwx------      3",
+	}
+	tests = append(tests, testStruct{flags: tempFl, outData: tempOD})
+
+	tempFl = Flags{"1", "окт", true}
+	tempOD = []string{
+		"drwxr-xr-x      7       user    user 4096",
+	}
+	tests = append(tests, testStruct{flags: tempFl, outData: tempOD})
+
+	for numTest, test := range tests {
+		waitData := cutFile(inputText, test.flags)
+		for ind, str := range test.outData {
+			if str != test.outData[ind] {
+				t.Error("Error in ", numTest)
+				t.Error(str, " -- ", waitData[ind])
+			}
+		}
+	}
+}
