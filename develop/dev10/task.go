4d0a437440346f0773d471c61219febddb29a308
Skripalschikov Ivan <kubing8@gmail.com>
2022-11-12 19:43:19 +0300

    task 10 complete

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +26,66 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
+/*
+Примеры:
+ateraan.com 4002
+mtrek.com 1701
+*/
 func main() {
+	timeoutFlag := flag.Int("timeout", 10, "Timeout")
+	flag.Parse()
 
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	timeout := time.Duration(*timeoutFlag) * time.Second
+
+	fmt.Println("Timeout:", timeout, "Host:", host, "Port:", port)
+
+	conn, err := net.DialTimeout("tcp", host+":"+port, timeout)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go netToStdout(conn, &wg)
+	go stdintToNet(conn, &wg)
+
+	wg.Wait()
+
+	defer fmt.Println("\nClose program")
+}
+
+// stdintToNet - читает из stdInt и пишет в net.Conn
+func stdintToNet(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		_, err := conn.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+	defer conn.Close() // if ctrl + D
+}
+
+// netToStdout - читает из net.Conn и пишет в Stdout
+func netToStdout(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	reader := bufio.NewReader(conn)
+
+	for {
+		message, err := reader.ReadString(' ')
+		if err == io.EOF { // if server closed connection
+			fmt.Println("\nConnection is closed")
+			os.Exit(0)
+		}
+		if netErr, ok := err.(net.Error); ok && !netErr.Timeout() { // if Scanner closed connection
+			break
+		}
+		fmt.Fprint(os.Stdout, message)
+	}
 }
