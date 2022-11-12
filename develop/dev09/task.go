157d8e48611013f02f287983b2464ed76f701311
Skripalschikov Ivan <kubing8@gmail.com>
2022-11-12 18:49:27 +0300

    task 9 complete

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита wget ===
 
@@ -8,6 +17,48 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Flags - флаги, поддерживаемые уилитой wget
+type Flags struct {
+	OutDir string
+}
 
+// Наверное нужна опци -О => директория вывода файла, если не указана, то выводить в текущую
+func main() {
+	outDir := flag.String("O", ".", "Use to specify the output directory")
+	flag.Parse()
+
+	fl := Flags{*outDir}
+	url := flag.Arg(0)
+
+	page := wget(url)
+	writeFile(fl, url, page)
+	//fmt.Print(string(page))
+}
+
+func writeFile(fl Flags, url string, strs []byte) {
+	var path string
+	url = strings.TrimPrefix(url, "https://")
+	domens := strings.Split(url, "/")
+	file := domens[len(domens)-1]
+
+	if fl.OutDir == "." {
+		path = "C:\\GoCode\\src\\WB_L2\\develop\\dev09\\" + file + ".html"
+	} else {
+		path = fl.OutDir + file + ".html"
+	}
+
+	if err := os.WriteFile(path,
+		strs, os.ModePerm); err != nil {
+		fmt.Println("Ошибка записи в файл.\n", err)
+	}
+}
+
+func wget(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	return body
 }
